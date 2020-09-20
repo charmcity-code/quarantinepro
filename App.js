7ceca5a37cb761cd782b62a38af3e5b957eb006a
Mark Lawrence <lawrence.ml@gmail.com>
2020-09-20 17:16:13 -0400

    load custom font

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
+
+const customFont = {
+  "Press-Start2p": require("./assets/fonts/PressStart2P-Regular.ttf"),
+};
 
 export default function App() {
+  const [isLoaded] = useFonts(customFont);
+
+  if (!isLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#ffbd12",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "Press-Start2p",
+          fontSize: 24,
+          marginTop: 80,
+          paddingHorizontal: 20,
+        }}
+      >
+        {`Are You a Quarantine Pro?`}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
